38540b001f654b9f9f400d3323b0c6d2e850aed5
mibrahim <politokuet2k2@gmail.com>
2019-01-09 01:12:58 +0100

    set up images and Array

--- src/app/components/product.js
+++ src/app/components/product.js
@@ -4,9 +4,33 @@ import ReactDOM from "react-dom";
 export class Product extends React.Component {
 	render() {
 		return(
-			<div>
-			   	<h1>Hello World!</h1>
-			</div>
+	       <div className="row">
+	        <div>
+	          <img src={this.props.productImageUrl} width="100" height="110"/>
+	        </div>
+	        <div className="col-sm-8">
+	          <div>
+	            <a>
+	              <i className="fa fa-caret-up" />
+	            </a>
+	            {this.props.votes}
+	          </div>
+	          <div>
+	            <a href={this.props.url}>
+	              {this.props.title}
+	            </a> 
+	            <p>
+	              {this.props.description}
+	            </p>
+	          </div>
+	          <div>
+	            <span>Submitted by:</span>
+	            <img
+	              className="rounded-circle" width="25" height="25"
+	              src={this.props.submitterAvatarUrl}/> 
+	           </div>
+	        </div>
+	      </div>
 		);
 	}
 }
--- src/app/components/productlist.js
+++ src/app/components/productlist.js
@@ -1,12 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Product } from "./product";
+import { seed } from "./seed";
 
 export class ProductList extends React.Component {
 	render() {
+        const products = Seed.products.sort((a,b) =>(
+        	    b.votes - a.votes
+        	));
+		const productComponents = Seed.products.map((product) => (
+			    <Product
+			      key={"product-" + product.id}
+                  id={product.id}
+                  title={product.title}
+                  description={product.description}
+                  url={product.url}
+                  votes={product.votes}
+                  submitterAvatarUrl={product.submitterAvatarUrl}
+                  productImageUrl={product.productImageUrl}
+               />
+		));
 		return(
-			<div>
-             <Product />
+			<div className="container">
+              {productComponents}
 			</div>
 		);
 	}
